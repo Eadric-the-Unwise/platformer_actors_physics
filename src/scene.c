31c809eebb66b98926e1ff08e3997ccad855864c
Eric Laptop <laptop@example.com>
2021-11-03 13:49:27 +0900

    holy shit NPCs are loading/offloading properly!

--- src/scene.c
+++ src/scene.c
@@ -62,7 +62,7 @@ void render_actors() {
         if (current_animation != NULL) {
             if (current_animation[current_actor->animation_phase] != NULL) {
                 //HERE MUST BE ALL NPC BUT THE PLAYER//
-                if (NPC_PLAYER_Offset <= 160 || NPC_PLAYER_Offset >= -160) {
+                if (NPC_PLAYER_Offset <= 160 || NPC_PLAYER_Offset >= -100) {
                     if ((current_direction == DIR_RIGHT) || (current_direction == DIR_JUMP_R) || (current_direction == DIR_IDLE_R) || (current_direction == DIR_DOWN_R) || (current_direction == DIR_CRAWL_R)) {
                         hiwater += move_metasprite_vflip(
                             current_animation[current_actor->animation_phase],
@@ -77,9 +77,11 @@ void render_actors() {
                             TO_PIXELS(current_actor->x), TO_PIXELS(current_actor->y));
                     }
                 }
-                else hide_metasprite(
+                else { 
+                    // hiwater -= current_actor->metasprite_count;
+                    hide_metasprite(
                     current_animation[current_actor->animation_phase],
-                    hiwater);
+                    hiwater);}
             }
             // process actor animation
             if (animation_timer == 1) {
