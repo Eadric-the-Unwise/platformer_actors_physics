fee815e09d9054a48ebca01596c44d694adbc1e7
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2021-11-22 16:58:13 +0900

    updated crawl and jump animations

--- res/tiles/detective_large.h
+++ res/tiles/detective_large.h
@@ -5,9 +5,8 @@
 #define detective_large_PIVOT_X 12
 #define detective_large_PIVOT_Y 16
 
-extern const UINT8 detective_large_data[2112];
-//extern const metasprite_t* const detective_large_metasprites[];
-
+extern const UINT8 detective_large_data[2240];
+extern const metasprite_t* const detective_large_metasprites[15];
 extern const metasprite_t* const detective_walk_left[];
 extern const metasprite_t* const detective_crouch[];
 extern const metasprite_t* const detective_crawl_left[];
