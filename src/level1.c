9dc84cc41969adba6eda4a3054ad3f9d87481f21
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2021-12-02 21:40:56 +0900

    updated drop_map

--- src/level1.c
+++ src/level1.c
@@ -14,10 +14,10 @@ extern UINT8 joy, last_joy;
 UINT8 Attach, x_Collide, y_Collide;
 UINT8 current_elevator;
 
-//CURRENTLY, LOADING FROM THE RIGHT FORCES YOU TO CALC (X COORD MINUS THE TO_PIXELS(CAM.X)). IS THERE A WAY TO AUTOMATICALLY CAL THIS VALUE UPON LOAD?
+// CURRENTLY, LOADING FROM THE RIGHT FORCES YOU TO CALC (X COORD MINUS THE TO_PIXELS(CAM.X)). IS THERE A WAY TO AUTOMATICALLY CAL THIS VALUE UPON LOAD?
 //.w and .h are adjusted for COLLISION functions
-const actor_t level1_actors[7] = {
-    //0 PLAYER
+const actor_t level1_actors[5] = {
+    // 0 PLAYER
     {.x = TO_COORDS(136),
      .y = TO_COORDS(-8),
      .SpdX = 0,
@@ -35,8 +35,8 @@ const actor_t level1_actors[7] = {
      .animations_props = {ANIM_LOOP, ANIM_LOOP, ANIM_ONCE, ANIM_ONCE, ANIM_LOOP, ANIM_LOOP, ANIM_ONCE, ANIM_ONCE, ANIM_ONCE, ANIM_ONCE, ANIM_ONCE, ANIM_ONCE, ANIM_ONCE, ANIM_ONCE},
      .animation_phase = 0,
      .copy = FALSE},
-    //1 NPC
-    {.x = TO_COORDS(-16),
+    // 1 NPC
+    {.x = TO_COORDS(-24),
      .y = TO_COORDS(148),
      .SpdX = 7,
      .SpdY = 0,
@@ -55,9 +55,9 @@ const actor_t level1_actors[7] = {
      .animations_props = {ANIM_LOOP, ANIM_LOOP},
      .animation_phase = 0,
      .copy = FALSE},
-    //2 NPC
-    {.x = TO_COORDS(-160),
-     .y = TO_COORDS(84),
+    // 2 NPC
+    {.x = TO_COORDS(-168),
+     .y = TO_COORDS(116),
      .SpdX = 7,
      .SpdY = 0,
      .w = NPC_electric_WIDTH,
@@ -75,7 +75,7 @@ const actor_t level1_actors[7] = {
      .animations_props = {ANIM_LOOP, ANIM_LOOP},
      .animation_phase = 0,
      .copy = TRUE},
-    //TOP PATROL
+    // TOP PATROL
     {.x = TO_COORDS(56),
      .y = TO_COORDS(68),
      .SpdX = -12,
@@ -97,7 +97,7 @@ const actor_t level1_actors[7] = {
      .animations_props = {ANIM_LOOP, ANIM_LOOP},
      .animation_phase = 0,
      .copy = TRUE},
-    //BOTTOM PATROL
+    // BOTTOM PATROL
     {.x = TO_COORDS(56),
      .y = TO_COORDS(132),
      .SpdX = 12,
@@ -118,59 +118,61 @@ const actor_t level1_actors[7] = {
      .animations = {NPC_electric_animation, NPC_electric_animation},
      .animations_props = {ANIM_LOOP, ANIM_LOOP},
      .animation_phase = 0,
-     .copy = TRUE},
-    // ELEVATOR
-    {.x = TO_COORDS(-218),
-     .y = TO_COORDS(104),
-     .SpdX = 0,
-     .SpdY = 16,
-     .w = vertical_platform_V1_WIDTH,
-     .h = vertical_platform_V1_HEIGHT,
-     .x_pivot = vertical_platform_V1_PIVOT_X,
-     .y_pivot = vertical_platform_V1_PIVOT_Y,
-     .x_offset = vertical_platform_V1_PIVOT_X,
-     .y_offset = vertical_platform_V1_PIVOT_Y,
-     .direction = DIR_DOWN,
-     .NPC_type = ELEVATOR,
-     .tile_count = (sizeof(vertical_platform_V1_data) >> 4),
-     .tile_index = 0,
-     .tile_data = vertical_platform_V1_data,
-     .patrol_timer = 64,
-     .patrol_reset = 64,
-     .animations = {elevator_frame, elevator_frame, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, elevator_frame, elevator_frame},
-     .animations_props = {ANIM_ONCE, ANIM_ONCE, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
-     .animation_phase = 0,
-     .copy = FALSE},
-    {.x = TO_COORDS(-298),
-     .y = TO_COORDS(88),
-     .SpdX = 0,
-     .SpdY = 12,
-     .w = vertical_platform_V1_WIDTH,
-     .h = vertical_platform_V1_HEIGHT,
-     .x_pivot = vertical_platform_V1_PIVOT_X,
-     .y_pivot = vertical_platform_V1_PIVOT_Y,
-     .x_offset = vertical_platform_V1_PIVOT_X,
-     .y_offset = vertical_platform_V1_PIVOT_Y,
-     .direction = DIR_DOWN,
-     .NPC_type = ELEVATOR,
-     .tile_count = (sizeof(vertical_platform_V1_data) >> 4),
-     .tile_index = 0,
-     .tile_data = vertical_platform_V1_data,
-     .patrol_timer = 1,
-     .patrol_reset = 64,
-     .animations = {elevator_frame, elevator_frame, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, elevator_frame, elevator_frame},
-     .animations_props = {ANIM_ONCE, ANIM_ONCE, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
-     .animation_phase = 0,
-     .copy = TRUE}};
+     .copy = TRUE}
+    //  ,
+    // // ELEVATOR
+    // {.x = TO_COORDS(-218),
+    //  .y = TO_COORDS(104),
+    //  .SpdX = 0,
+    //  .SpdY = 16,
+    //  .w = vertical_platform_V1_WIDTH,
+    //  .h = vertical_platform_V1_HEIGHT,
+    //  .x_pivot = vertical_platform_V1_PIVOT_X,
+    //  .y_pivot = vertical_platform_V1_PIVOT_Y,
+    //  .x_offset = vertical_platform_V1_PIVOT_X,
+    //  .y_offset = vertical_platform_V1_PIVOT_Y,
+    //  .direction = DIR_DOWN,
+    //  .NPC_type = ELEVATOR,
+    //  .tile_count = (sizeof(vertical_platform_V1_data) >> 4),
+    //  .tile_index = 0,
+    //  .tile_data = vertical_platform_V1_data,
+    //  .patrol_timer = 64,
+    //  .patrol_reset = 64,
+    //  .animations = {elevator_frame, elevator_frame, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, elevator_frame, elevator_frame},
+    //  .animations_props = {ANIM_ONCE, ANIM_ONCE, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+    //  .animation_phase = 0,
+    //  .copy = FALSE},
+    // {.x = TO_COORDS(-298),
+    //  .y = TO_COORDS(88),
+    //  .SpdX = 0,
+    //  .SpdY = 12,
+    //  .w = vertical_platform_V1_WIDTH,
+    //  .h = vertical_platform_V1_HEIGHT,
+    //  .x_pivot = vertical_platform_V1_PIVOT_X,
+    //  .y_pivot = vertical_platform_V1_PIVOT_Y,
+    //  .x_offset = vertical_platform_V1_PIVOT_X,
+    //  .y_offset = vertical_platform_V1_PIVOT_Y,
+    //  .direction = DIR_DOWN,
+    //  .NPC_type = ELEVATOR,
+    //  .tile_count = (sizeof(vertical_platform_V1_data) >> 4),
+    //  .tile_index = 0,
+    //  .tile_data = vertical_platform_V1_data,
+    //  .patrol_timer = 1,
+    //  .patrol_reset = 64,
+    //  .animations = {elevator_frame, elevator_frame, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, elevator_frame, elevator_frame},
+    //  .animations_props = {ANIM_ONCE, ANIM_ONCE, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+    //  .animation_phase = 0,
+    //  .copy = TRUE}
+};
 
 const level_t level1 = {
     .actors = level1_actors,
-    .actor_count = 7,
+    .actor_count = 5,
     .animate_hook = render_level1  // function that put life into the scene
 };
 
 void render_level1() {
-    actor_t *current_actor = &active_actors[ACTOR_FIRST_NPC];  //The Detective is currently active_actors[0], so active_actors[1] and above are enemies
+    actor_t *current_actor = &active_actors[ACTOR_FIRST_NPC];  // The Detective is currently active_actors[0], so active_actors[1] and above are enemies
 
     for (UINT8 i = active_actors_count - 1; i != 0; i--) {
         if ((TO_PIXELS(bkg.camera_x) > 0) && (TO_PIXELS(bkg.camera_x) < bkg.camera_max_x)) {
@@ -206,27 +208,27 @@ void render_level1() {
             }
         } else if (current_actor->NPC_type == WALK) {
             INT16 actor_x = TO_PIXELS(current_actor->x);
-            //NEED TO CALCULATE THE BKG.MAPS X POSITION RELATIVE TO THE SPAWN POSITION
-            // if (actor_x > 160){
-            //         current_actor->x= -60;
-            //         current_actor->ON = TRUE;
-            //     }
+            // NEED TO CALCULATE THE BKG.MAPS X POSITION RELATIVE TO THE SPAWN POSITION
+            //  if (actor_x > 160){
+            //          current_actor->x= -60;
+            //          current_actor->ON = TRUE;
+            //      }
             if (actor_x >= -40) {
                 current_actor->x += current_actor->SpdX;
             }
         }
         current_actor++;
     }
-    //WE SHOULD ONLY NEED TO CHECK FOR CROUCH OR JUMP, BECAUSE BOTH WALK AND LAND HAVE THE SAME HITBOXES. SET THE VALUES FOR EACH BOX HERE
-    if (Crouch) {  //CROUCH HITBOX
+    // WE SHOULD ONLY NEED TO CHECK FOR CROUCH OR JUMP, BECAUSE BOTH WALK AND LAND HAVE THE SAME HITBOXES. SET THE VALUES FOR EACH BOX HERE
+    if (Crouch) {  // CROUCH HITBOX
         PLAYER.h_offset = -2;
         PLAYER.x_offset = 8;
         PLAYER.y_offset = 16;
-    } else if (PLAYER.SpdY != 0) {  //JUMP AND FALLING HITBOX
+    } else if (PLAYER.SpdY != 0) {  // JUMP AND FALLING HITBOX
         PLAYER.h_offset = 11;
         PLAYER.x_offset = 6;
         PLAYER.y_offset = 6;
-    } else if ((!Crouch) && (!Jump)) {  //STAND AND WALKING HITBOX
+    } else if ((!Crouch) && (!Jump)) {  // STAND AND WALKING HITBOX
         PLAYER.h_offset = 7;
         PLAYER.x_offset = 6;
         PLAYER.y_offset = 16;
@@ -234,22 +236,22 @@ void render_level1() {
 }
 
 void npc_collisions_level1() {
-    //CHECK LANDING HOTBOX TIMING
+    // CHECK LANDING HOTBOX TIMING
     for (UINT8 i = ACTOR_FIRST_NPC; i != (active_actors_count); i++) {
         //[y][x]
         UINT16 PTR_y, PTR_x, PBL_y, PBL_x, NTR_y, NTR_x, NBL_y, NBL_x;
         UINT8 ax, ay;
         ax = TO_PIXELS(active_actors[i].x);
         ay = TO_PIXELS(active_actors[i].y);
-        //THE PIVOT IS THE LITERAL CENTER OF THE METASPRITE. NOT A PIXEL, BUT THE CROSSHAIRS IN THE MIDDLE OF THE DESGIN
-        PTR_y = py - PLAYER.h_offset;            //TR y
-        PTR_x = px + PLAYER.x_offset;            //TR x
-        PBL_y = py + PLAYER.y_offset;            //BL y
-        PBL_x = px - PLAYER.x_offset;            //BL x
-        NTR_y = ay - active_actors[i].y_offset;  //TR y
-        NTR_x = ax + active_actors[i].x_offset;  //TR x
-        NBL_y = ay + active_actors[i].y_offset;  //BL y
-        NBL_x = ax - active_actors[i].x_offset;  //BL x
+        // THE PIVOT IS THE LITERAL CENTER OF THE METASPRITE. NOT A PIXEL, BUT THE CROSSHAIRS IN THE MIDDLE OF THE DESGIN
+        PTR_y = py - PLAYER.h_offset;            // TR y
+        PTR_x = px + PLAYER.x_offset;            // TR x
+        PBL_y = py + PLAYER.y_offset;            // BL y
+        PBL_x = px - PLAYER.x_offset;            // BL x
+        NTR_y = ay - active_actors[i].y_offset;  // TR y
+        NTR_x = ax + active_actors[i].x_offset;  // TR x
+        NBL_y = ay + active_actors[i].y_offset;  // BL y
+        NBL_x = ax - active_actors[i].x_offset;  // BL x
 
         if (overlap(PTR_y, PTR_x, PBL_y, PBL_x, NTR_y, NTR_x, NBL_y, NBL_x) == 0x01U) {
             if (active_actors[i].NPC_type != ELEVATOR) {
@@ -262,7 +264,7 @@ void npc_collisions_level1() {
                 }
             } else if (active_actors[i].NPC_type == ELEVATOR) {
                 if (active_actors[i].ON == TRUE) {
-                    if ((PBL_x > NTR_x - 2) || (PTR_x < NBL_x + 2))  //is not on top of elevator
+                    if ((PBL_x > NTR_x - 2) || (PTR_x < NBL_x + 2))  // is not on top of elevator
                     {
                         x_Collide = TRUE;
                     } else if ((PBL_y > NTR_y) && (PBL_y < NBL_y)) {
--- src/level1.h
+++ src/level1.h
@@ -3,7 +3,7 @@
 #include "level.h"
 #include "scene.h"
 
-extern const struct actor_t level1_actors[7];
+extern const struct actor_t level1_actors[5];
 extern const struct level_t level1;
 void npc_collisions_level1();
 void render_level1();
