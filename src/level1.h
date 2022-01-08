24f12869947222c713ea989fddbb4685e8de9449
Eric Laptop <laptop@example.com>
2022-01-08 14:55:09 +0900

    pre LVL2

--- src/level1.h
+++ src/level1.h
@@ -4,14 +4,14 @@
 #include "scene.h"
 #define LEVEL1_BANK 5
 extern const struct actor_t level1_actors[5];
-extern UINT8 cam1[3];
-extern UINT8 cam2[2];
-extern UINT8 cam3[2];
-extern UINT8 cam4[1];
 extern UINT8 cam1_render[4];
 extern UINT8 cam2_render[3];
 extern UINT8 cam3_render[3];
 extern UINT8 cam4_render[2];
+extern UINT8 cam1[3];
+extern UINT8 cam2[2];
+extern UINT8 cam3[2];
+extern UINT8 cam4[1];
 extern const struct level_t level1;
 extern UINT8 render_actors_count;
 void npc_collisions_level1();
