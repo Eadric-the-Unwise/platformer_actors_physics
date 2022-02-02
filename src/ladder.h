5e56bfbbdcefd9aa9aa2bc3cf441001fd17d45b2
Eric Laptop <laptop@example.com>
2022-02-02 15:11:15 +0900

    EOD

--- src/ladder.h
+++ src/ladder.h
@@ -1,7 +1,7 @@
 #include "collisions.h"
 #include "scene.h"
 
-void check_LADDER(UINT8 newplayerx, UINT8 newplayery, INT16 camera_x);
+void check_LADDER(UINT8 newplayerx, UINT8 newplayery, INT16 camera_x, const unsigned char *COLLISION_DATA, UINT8 COLLISION_DATAWidth);
 // extern UINT8 ONTO_Ladder_timer, DOWN_LADDER_timer, UP_LADDER_timer, OFF_LADDER_timer;
 extern UINT8 LADDER, ONTO_Ladder, DOWN_LADDER, DOWN_LADDER_F, UP_LADDER, OFF_LADDER, LADDER_Release;
 void ladder();
