deea0515241a59f034e38e7662fba9cbd6e70d71
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2022-02-03 12:51:50 +0900

    added debugger

--- make.bat
+++ make.bat
@@ -2,8 +2,9 @@ call .\clean.bat
 
 @echo off
 @REM ENTER YOUR GBDK DIRECTORY IN @set GBDK = ...
+@REM -Wl-j .noi bgb debugging -Wm-yS
 @set GBDK=..\..\..\gbdk
-%GBDK%\bin\lcc.exe -autobank -Wl-j -Wm-yS -Wb-ext=.rel -Wb-v -Wl-yt0x1B -Wl-yoA -Wl-ya4 -o platformer_physics.gb^
+%GBDK%\bin\lcc.exe -autobank -Wf--debug -Wl-y -Wb-ext=.rel -Wb-v -Wl-yt0x1B -Wl-yoA -Wl-ya4 -o platformer_physics.gb^
  src\*.c res\tiles\*.c
  
 
